b72ff61620e1c977ec5f8eb4fae87288b4f1e1fc
CodewithAndare-hub <139978681+CodewithAndare-hub@users.noreply.github.com>
2024-06-17 11:04:17 +0300

    Update settings.py

--- Food_Delivery_System/settings.py
+++ Food_Delivery_System/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-$7mv%+t^8%9ygr55nhr$5vd5-(#02=&v*pg75x+^43o^n2s2s*
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['.vercel.app']
 
 # Application definition
 AUTHENTICATION_BACKENDS = [
